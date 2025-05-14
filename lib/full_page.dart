682e777290441bfda91a9e65a0303809633cc051
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-14 22:56:45 +0700

    Tambah fitur ambil dan tampil gambar

---
+++ lib/full_page.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class FullPage extends StatefulWidget {
+  const FullPage({super.key});
+
+  @override
+  State<FullPage> createState() => _FullPageState();
+}
