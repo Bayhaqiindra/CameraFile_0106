91d782913dc35206d898b749490cc2d0d4d1e8e3
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-14 23:42:46 +0700

    Tambah event SwitchCamera, ToggleFlash, dan TakePicture

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -4,3 +4,13 @@ import 'package:flutter/material.dart';
 sealed class CameraEvent {}
 
 final class InitializeCamera extends CameraEvent {}
+
+final class SwitchCamera extends CameraEvent {}
+
+final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+    final void Function(File imageFile) onPictureTaken;
+    TakePicture(this.onPictureTaken);
+}
+
