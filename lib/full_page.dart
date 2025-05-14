fa75dcf05366d36d8ff9f2c1184b2d3d57d892dd
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-14 22:57:44 +0700

    Menambahkan fungsi untuk meminta izin kamera dan penyimpanan

--- lib/full_page.dart
+++ lib/full_page.dart
@@ -10,3 +10,9 @@ class FullPage extends StatefulWidget {
 
 class _FullPageState extends State<FullPage> {
   File? _imageFile;
+
+Future<void> _requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.storage.request();
+    await Permission.manageExternalStorage.request();
+  }
