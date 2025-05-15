83e05b1dd9864a17350c8ecc6709473a43c2bfe0
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-15 22:18:50 +0700

    buat helper untuk simpan gambar ke folder lokal dengan penamaan unik

---
+++ lib/services/storage_helper.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+class StorageHelper {
+    static Future<String> __getFolderPath() async {
+        final dir = Directory('/storage/emulated/0/DCTM/FlutterNativeCamBloc');
+        if (!await dir.exists()) {
+            await dir.create(recursive: true);
+        }
+        return dir.path;
+    }
+
+    static Future<File> saveImage(File file, String prefix) async {
+        final dirPath = await __getFolderPath();
+        final fileName = '$prefix${DateTime.now().millisecondsSinceEpoch}${path.extension(file.path)}';
+        final savedPath = path.join(dirPath, fileName);
+        return await file.copy(savedPath);
+    }
+}
