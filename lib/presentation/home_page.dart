ed450e4d80ab7913c4b2da3cf48d8af62afff945
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-15 22:17:17 +0700

    buat halaman HomePage dan siapkan integrasi dengan CameraBloc

---
+++ lib/presentation/home_page.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pertemuan_7/bloc/camera_bloc.dart';
+import 'package:pertemuan_7/bloc/camera_event.dart';
+import 'package:pertemuan_7/bloc/camera_state.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
