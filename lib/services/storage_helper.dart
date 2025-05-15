ff4b1e4ddfb8924c1eb26220a0aecf89824f821d
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-15 22:25:20 +0700

    fixed code

--- lib/services/storage_helper.dart
+++ lib/services/storage_helper.dart
@@ -2,16 +2,15 @@ import 'dart:io';
 import 'package:path/path.dart' as path;
 
 class StorageHelper {
-    static Future<String> __getFolderPath() async {
+    static Future<String> _getFolderPath() async {
         final dir = Directory('/storage/emulated/0/DCTM/FlutterNativeCamBloc');
-        if (!await dir.exists()) {
+        if (!await dir.exists()) 
             await dir.create(recursive: true);
-        }
         return dir.path;
     }
 
     static Future<File> saveImage(File file, String prefix) async {
-        final dirPath = await __getFolderPath();
+        final dirPath = await _getFolderPath();
         final fileName = '$prefix${DateTime.now().millisecondsSinceEpoch}${path.extension(file.path)}';
         final savedPath = path.join(dirPath, fileName);
         return await file.copy(savedPath);
