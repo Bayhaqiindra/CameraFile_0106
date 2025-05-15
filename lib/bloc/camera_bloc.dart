925a033aaa63554792bc83960d781ec46ed3a212
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-15 19:46:41 +0700

    implementasi CameraBloc dengan event handler kamera dan galeri

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:pertemuan_7/presentation/camera_page.dart';
+import 'package:pertemuan_7/services/storage_helper.dart';
 
-part 'camera_event.dart';
-part 'camera_state.dart';
+import 'camera_event.dart';
+import 'camera_state.dart';
 
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
-  CameraBloc() : super(CameraInitial()) {
-    on<CameraEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
-}
+  late final List<CameraDescription> _cameras;
+
+  
